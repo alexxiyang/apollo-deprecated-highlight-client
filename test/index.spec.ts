cfa5bbe412a665b7b20abe6e8c55ce9fba24bb36
alexy <alex.yang@ehealth.com>
2023-01-04 23:52:57 -0800

    add unit test, circleci

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,6 +1,38 @@
+import { ApolloDeprecatedHighlightClient } from '../src/index'
+
 describe('ApolloDeprecatedHighlightClient', () => {
+    beforeEach(() => {
+        jest.resetAllMocks();
+    })
+
     it('should print out console warn', () => {
-        // TODO:
-        expect(1).toBe(1);
+        const warn = jest.spyOn(console, "warn").mockImplementation(() => {});
+        const operation = {
+            operationName: 'bar'
+        };
+        const middleware = ApolloDeprecatedHighlightClient(operation);
+        const response = {
+            extensions: {
+                deprecations: [
+                    {
+                        field: 'foo',
+                        reason: 'foo is deprecated!'
+                    }
+                ]
+            }
+        };
+        expect(middleware(response))
+        expect(warn).toBeCalledWith('🚨 foo in bar is deprecated. Because foo is deprecated!');
+    })
+
+    it('should not print out console warn if there is no extensions', () => {
+        const warn = jest.spyOn(console, "warn").mockImplementation(() => {});
+        const operation = {
+            operationName: 'bar'
+        };
+        const middleware = ApolloDeprecatedHighlightClient(operation);
+        const response = {};
+        expect(middleware(response))
+        expect(warn).not.toHaveBeenCalled();
     })
 })
